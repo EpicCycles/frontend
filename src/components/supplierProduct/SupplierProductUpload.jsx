73e162d760dc1a9b34daee7f41782d5db93fe77b
Anna <appdev.epiccycles@gmail.com>
2019-06-07 17:13:30 +0100

    supplier product use part type thing

--- src/components/supplierProduct/SupplierProductUpload.jsx
+++ src/components/supplierProduct/SupplierProductUpload.jsx
@@ -1,172 +1,200 @@
-import React, {Fragment} from "react";
-import SupplierProductUploadFile from "./SupplierProductUploadFile";
-import {doesFieldMatchPartType} from "../partType/helpers/partType";
-import {colourStyles} from "../../helpers/constants";
-import UploadMappingPartTypes from "../app/upload/UploadMappingPartTypes";
-import UploadMappingSuppliers from "../app/upload/UploadMappingSuppliers";
-import {buildSupplierProductForApi} from "./helpers/supplierProduct";
-import BrandPrompt from "../brand/BrandPrompt";
-import SupplierProductUploadReview from "./SupplierProductUploadReview";
-import {doWeHaveObjects, updateObject} from "../../helpers/utils";
+import React, { Fragment } from 'react';
+import SupplierProductUploadFile from './SupplierProductUploadFile';
+import { getPartTypeForName } from '../partType/helpers/partType';
+import { colourStyles } from '../../helpers/constants';
+import UploadMappingPartTypes from '../app/upload/UploadMappingPartTypes';
+import UploadMappingSuppliers from '../app/upload/UploadMappingSuppliers';
+import { buildSupplierProductForApi } from './helpers/supplierProduct';
+import BrandPrompt from '../brand/BrandPrompt';
+import SupplierProductUploadReview from './SupplierProductUploadReview';
+import { doWeHaveObjects, removeKey, updateObject } from '../../helpers/utils';
 
 const uploadSteps = [
-    {
-        stepDescriptor: "File",
-        description: "Upload File"
-    },
-    { stepDescriptor: "Part Types", description: "Assign Part Types for upload data" },
-    { stepDescriptor: "Suppliers", description: "Assign Suppliers for upload data" },
-    { stepDescriptor: "Brands", description: "Add missing brands" },
-    { stepDescriptor: "Review", description: "Review data for parts to be created during upload" },
+  {
+    stepDescriptor: 'File',
+    description: 'Upload File',
+  },
+  { stepDescriptor: 'Part Types', description: 'Assign Part Types for upload data' },
+  { stepDescriptor: 'Suppliers', description: 'Assign Suppliers for upload data' },
+  { stepDescriptor: 'Brands', description: 'Add missing brands' },
+  { stepDescriptor: 'Review', description: 'Review data for parts to be created during upload' },
 ];
 const initialState = {
-    step: 0
+  step: 0,
 };
 
 class SupplierProductUpload extends React.Component {
-    state = initialState;
+  state = initialState;
 
-    onChangeField = (fieldName, fieldValue) => {
-        let newState = this.state;
-        newState[fieldName] = fieldValue;
+  onChangeField = (fieldName, fieldValue) => {
+    let newState = this.state;
+    newState[fieldName] = fieldValue;
 
-        this.setState(newState);
-    };
+    this.setState(newState);
+  };
 
-    addDataAndProceed = (dataForState) => {
-        let nextStep = this.state.step + 1;
+  addDataAndProceed = dataForState => {
+    let nextStep = this.state.step + 1;
 
-        let newState = updateObject(this.state, dataForState, { step: nextStep });
-        if (newState.rowMappings) {
-            const missingPartType = newState.rowMappings.some(rowMapping => (!rowMapping.partType && !rowMapping.ignore));
-            const missingSupplier = newState.rowMappings.some(rowMapping => (!rowMapping.supplier && !rowMapping.ignore));
-            if (missingPartType) {
-                newState.step = 1;
-            } else if (missingSupplier) {
-                newState.step = 2;
-            } else {
-                newState.step = 3;
-                newState.apiData = buildSupplierProductForApi(newState.rowMappings, newState.uploadedData, this.props.brands);
-                if (doWeHaveObjects(newState.apiData.updatedBrands)) this.props.saveBrands(newState.apiData.updatedBrands);
-                if (!doWeHaveObjects(newState.apiData.partsMissingBrands)) newState.step = 4;
-            }
+    let newState = updateObject(this.state, dataForState, { step: nextStep });
+    if (newState.rowMappings) {
+      const missingPartType = newState.rowMappings.some(
+        rowMapping => !rowMapping.partType && !rowMapping.ignore,
+      );
+      const missingSupplier = newState.rowMappings.some(
+        rowMapping => !rowMapping.supplier && !rowMapping.ignore,
+      );
+      if (missingPartType) {
+        newState.step = 1;
+      } else if (missingSupplier) {
+        newState.step = 2;
+      } else {
+        newState.step = 3;
+        newState.apiData = buildSupplierProductForApi(
+          newState.rowMappings,
+          newState.uploadedData,
+          this.props.brands,
+        );
+        if (doWeHaveObjects(newState.apiData.updatedBrands))
+          this.props.saveBrands(newState.apiData.updatedBrands);
+        if (!doWeHaveObjects(newState.apiData.partsMissingBrands)) newState.step = 4;
+      }
+    }
+
+    this.setState(newState);
+  };
+
+  goToStep = step => {
+    if (step < this.state.step) {
+      let newState = updateObject(this.state, { step: step });
+      this.setState(newState);
+    }
+  };
+  startAgain = () => {
+    this.props.clearParts();
+    this.setState(initialState);
+  };
+  buildInitialRowMappings = uploadedData => {
+    const { sections, suppliers } = this.props;
+    return uploadedData.map((row, rowIndex) => {
+      const partTypeName = row[0].trim();
+      const partType = getPartTypeForName(sections, partTypeName);
+      const supplierName = row[1].trim();
+      const supplierNameLower = supplierName.toLowerCase();
+      const rowData = { rowIndex, partTypeName, supplierName };
+
+      if (partType) {
+        rowData.partType = partType.id;
+      }
+
+      suppliers.some(supplier => {
+        if (supplier.supplier_name.toLowerCase() === supplierNameLower) {
+          rowData.supplier = supplier.id;
+          return true;
         }
+        return false;
+      });
+      return rowData;
+    });
+  };
 
-        this.setState(newState);
-    };
+  actionOnBrandsChange = () => {
+    if (!this.props.isLoading) {
+      let newState = updateObject(this.state);
+      newState.apiData = buildSupplierProductForApi(
+        newState.rowMappings,
+        newState.uploadedData,
+        this.props.brands,
+      );
+      if (doWeHaveObjects(newState.apiData.updatedBrands))
+        this.props.saveBrands(newState.apiData.updatedBrands);
+      if (!doWeHaveObjects(newState.apiData.partsMissingBrands)) newState.step = 4;
+      this.setState(newState);
+    }
+  };
 
-    goToStep = (step) => {
-        if (step < this.state.step) {
-            let newState = updateObject(this.state, { step: step });
-            this.setState(newState);
-        }
-    };
-    startAgain = () => {
-        this.props.clearParts();
-        this.setState(initialState);
-    };
-    buildInitialRowMappings = (uploadedData) => {
-        const { sections, suppliers } = this.props;
-        return uploadedData.map((row, rowIndex) => {
-            const partTypeName = row[0].trim();
-            const partTypeNameLower = partTypeName.toLowerCase();
-            const supplierName = row[1].trim();
-            const supplierNameLower = supplierName.toLowerCase();
-            const rowData = { rowIndex, partTypeName, supplierName };
-
-            sections.some(section => {
-                return section.partTypes.some(partType => {
-                    if (doesFieldMatchPartType(partType, partTypeNameLower)) {
-                        rowData.partType = partType.id;
-                        return true;
-                    }
-                    return false;
-                });
-            });
-
-            suppliers.some(supplier => {
-                if (supplier.supplier_name.toLowerCase() === supplierNameLower) {
-                    rowData.supplier = supplier.id;
-                    return true;
-                }
-                return false;
-            });
-            return rowData
-        });
-    };
-
-    actionOnBrandsChange = () => {
-        if (!this.props.isLoading) {
-            let newState = updateObject(this.state);
-            newState.apiData = buildSupplierProductForApi(newState.rowMappings, newState.uploadedData, this.props.brands);
-            if (doWeHaveObjects(newState.apiData.updatedBrands)) this.props.saveBrands(newState.apiData.updatedBrands);
-            if (!doWeHaveObjects(newState.apiData.partsMissingBrands)) newState.step = 4;
-            this.setState(newState);
-        }
-    };
-
-    render() {
-        const { sections, suppliers, brands, saveFramework, saveSupplier, uploadParts, saveBrands } = this.props;
-        const { step, rowMappings, apiData } = this.state;
-        return <Fragment key="supplierProductUpload">
-            <h2>Supplier Product Upload - {uploadSteps[step].description}</h2>
-            {(step === 0) && <SupplierProductUploadFile
-                buildInitialRowMappings={this.buildInitialRowMappings}
-                addDataAndProceed={this.addDataAndProceed}
-            />}
-            {(step === 1) && <UploadMappingPartTypes
-                rowMappings={rowMappings}
-                sections={sections}
-                saveFramework={saveFramework}
-                addDataAndProceed={this.addDataAndProceed}
-                multiplesAllowed
-            />}
-            {(step === 2) && <UploadMappingSuppliers
-                rowMappings={rowMappings}
-                suppliers={suppliers}
-                saveSupplier={saveSupplier}
-                addDataAndProceed={this.addDataAndProceed}
-            />}
-            {(step === 3) && <BrandPrompt
-                brands={brands}
-                productDescriptions={apiData.partsMissingBrands}
-                saveBrands={saveBrands}
-                actionOnBrandsChange={this.actionOnBrandsChange}
-            />}
-            {(step === 4) && <SupplierProductUploadReview
-                apiData={apiData}
-                brands={brands}
-                sections={sections}
-                suppliers={suppliers}
-                uploadParts={uploadParts}
-            />}
-            <div className="full align_center">
-                {uploadSteps.map((stepDetails, stepIndex) => <Fragment>
-                        {(colourStyles[stepIndex].transition) && <div
-                            className={colourStyles[stepIndex].transition}
-                            key={`transition${stepIndex}`}
-                        />}
-                        <div
-                            key={`step${stepDetails.stepDescriptor}`}
-                            className={`circle
-                            ${(stepIndex === step)
-                                ? " selected-circle"
-                                : " unselected-circle"}
+  render() {
+    const {
+      sections,
+      suppliers,
+      brands,
+      saveFramework,
+      saveSupplier,
+      uploadParts,
+      saveBrands,
+    } = this.props;
+    const { step, rowMappings, apiData } = this.state;
+    return (
+      <Fragment key="supplierProductUpload">
+        <h2>Supplier Product Upload - {uploadSteps[step].description}</h2>
+        {step === 0 && (
+          <SupplierProductUploadFile
+            buildInitialRowMappings={this.buildInitialRowMappings}
+            addDataAndProceed={this.addDataAndProceed}
+          />
+        )}
+        {step === 1 && (
+          <UploadMappingPartTypes
+            rowMappings={rowMappings}
+            sections={sections}
+            saveFramework={saveFramework}
+            addDataAndProceed={this.addDataAndProceed}
+            multiplesAllowed
+          />
+        )}
+        {step === 2 && (
+          <UploadMappingSuppliers
+            rowMappings={rowMappings}
+            suppliers={suppliers}
+            saveSupplier={saveSupplier}
+            addDataAndProceed={this.addDataAndProceed}
+          />
+        )}
+        {step === 3 && (
+          <BrandPrompt
+            brands={brands}
+            productDescriptions={apiData.partsMissingBrands}
+            saveBrands={saveBrands}
+            actionOnBrandsChange={this.actionOnBrandsChange}
+          />
+        )}
+        {step === 4 && (
+          <SupplierProductUploadReview
+            apiData={apiData}
+            brands={brands}
+            sections={sections}
+            suppliers={suppliers}
+            uploadParts={uploadParts}
+          />
+        )}
+        <div className="full align_center">
+          {uploadSteps.map((stepDetails, stepIndex) => (
+            <Fragment>
+              {colourStyles[stepIndex].transition && (
+                <div
+                  className={colourStyles[stepIndex].transition}
+                  key={`transition${stepIndex}`}
+                />
+              )}
+              <div
+                key={`step${stepDetails.stepDescriptor}`}
+                className={`circle
+                            ${stepIndex === step ? ' selected-circle' : ' unselected-circle'}
                                  ${colourStyles[stepIndex].colour}
                                   ${colourStyles[stepIndex].background}
                                    ${colourStyles[stepIndex].border}`}
-                            onClick={() => this.goToStep(stepIndex)}
-                            disabled={!(stepIndex < this.state.step)}
-                            title={stepDetails.description}
-                        >
-                            {stepDetails.stepDescriptor}
-                        </div>
-                    </Fragment>
-                    )
-                }
-            </div>
-        </Fragment>
-    }
+                onClick={() => this.goToStep(stepIndex)}
+                disabled={!(stepIndex < this.state.step)}
+                title={stepDetails.description}
+              >
+                {stepDetails.stepDescriptor}
+              </div>
+            </Fragment>
+          ))}
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default SupplierProductUpload;
--- src/components/supplierProduct/SupplierProductUploadFile.jsx
+++ src/components/supplierProduct/SupplierProductUploadFile.jsx
@@ -1,137 +1,128 @@
-import React, {Fragment} from "react";
-import {Button} from "semantic-ui-react";
-import FormTextAreaInput from "../../common/FormTextAreaInput";
-import {supplierProductHeaders} from "./helpers/supplierProduct"
+import React, { Fragment } from 'react';
+import { Button } from 'semantic-ui-react';
+import FormTextAreaInput from '../../common/FormTextAreaInput';
+import { supplierProductHeaders } from './helpers/supplierProduct';
+import { handleFileUpload } from '../../helpers/upload';
 
 class SupplierProductUploadFile extends React.Component {
-    state = {};
+  state = {};
 
-    goToNextStep = () => {
-        const { uploadedData } = this.state;
-        const rowMappings = this.props.buildInitialRowMappings(uploadedData);
-        this.props.addDataAndProceed({ uploadedData, rowMappings });
+  goToNextStep = () => {
+    const { uploadedData } = this.state;
+    const rowMappings = this.props.buildInitialRowMappings(uploadedData);
+    this.props.addDataAndProceed({ uploadedData, rowMappings });
+  };
 
+  handleFileChosen = supplierProductUploadFile => {
+    let fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      const uploadResults = handleFileUpload(fileReader);
+      if (uploadResults) this.setState({ uploadedData: uploadResults.uploadedData });
     };
-
-    handleFileChosen = (supplierProductUploadFile) => {
-        let fileReader = new FileReader();
-        fileReader.onloadend = () => {
-            const fileContent = fileReader.result;
-            let fileLines = fileContent.split(/\r?\n/);
-            let uploadedData = [];
-            fileLines.forEach(fileLine => uploadedData.push(fileLine.split(',')));
-            if (uploadedData.length > 0) {
-                uploadedData.shift(); // remove header row
-                const usableData = uploadedData.filter(uploadRow => {
-                    if (uploadRow[0].length === 0) return false;
-
-                    const joinedData = uploadRow.join('');
-                    return (joinedData.length > (uploadRow[0].length + 1));
-                });
-                this.setState({ uploadedData: usableData });
-            }
-        };
-        fileReader.readAsText(supplierProductUploadFile);
-    };
-    processTextArea = (textInput) => {
-        let partLines = textInput.split("\n");
-        let uploadedData = [];
-        let splitCharacter = "\t";
-        partLines.forEach(partLine => uploadedData.push(partLine.split(splitCharacter)));
-        if (uploadedData.length > 0) {
-            this.setState({ uploadedData });
-        }
-    };
-    clearUploadData = () => {
-        this.setState({ uploadedData: [] });
-    };
-
-    render() {
-        const { modelName, uploadedData } = this.state;
-        const uploadData = (uploadedData && (uploadedData.length > 0));
-        const uploadDisabled = false;
-        return <Fragment key="supplierProductUploadFile">
-            {(!uploadData) && <div key='supplierProductUploadInput' className="grid">
-                <div className="grid-row">
-                    <div className="grid-item--borderless field-label red align_right">
-                        Either:
-                    </div>
-                    <div className="grid-item--borderless field-label">
-                        Select file for upload
-                    </div>
-                    <div className="grid-item--borderless">
-                        <input
-                            type='file'
-                            id='supplierProductUploadFile'
-                            accept='.csv'
-                            onChange={event => this.handleFileChosen(event.target.files[0])}
-                            disabled={uploadDisabled}
-                        />
-                    </div>
-                </div>
-                <div className="grid-row">
-                    <div className="grid-item--borderless field-label red align_right">
-                        Or:
-                    </div>
-                    <div className="grid-item--borderless field-label">
-                        Paste a list of supplier products
-                    </div>
-                    <div className="grid-item--borderless">
-                        <FormTextAreaInput
-                            title="Paste list of supplier part types and values here"
-                            placeholder={"e.g. Bars Deda 30cm"}
-                            onChange={this.processTextArea}
-                            disabled={!modelName}
-                            rows={20}
-                        />
-                    </div>
-                </div>
-            </div>}
-            {uploadData && <Fragment>
-                <div>
-                    <Button
-                        key="supplierProductFileUploadCont"
-                        onClick={this.goToNextStep}
-                    >
-                        Continue ...
-                    </Button>
-                    <Button
-                        key="supplierProductFileUploadReset"
-                        onClick={this.clearUploadData}
-                    >
-                        Clear data
-                    </Button>
-                </div>
-                <div
-                    key='supplierProductUploadGrid'
-                    className="grid"
-                    style={{
-                        height: (window.innerHeight - 100) + "px",
-                        width: (window.innerWidth - 50) + "px",
-                        overflow: "scroll"
-                    }}
-                >
-                    <div key="supplierProductUploadHeaders" className="grid-row grid-row--header">
-                        {supplierProductHeaders.map((cell, index) => <div
-                            key={`col1${index}`}
-                            className={(index === 0) ? "grid-item--header grid-header--fixed-left" : "grid-item--header"}
-                        >
-                            {cell}
-                        </div>)}
-                    </div>
-                    {uploadedData.map((uploadedRow, rowIndex) => <div key={`detailRow${rowIndex}`} className="grid-row">
-                        {uploadedRow.map((cell, index) => <div
-                            className={(index === 0) ? "grid-item grid-item--fixed-left" : "grid-item"}
-                            key={`row${rowIndex}col${index}`}
-                        >
-                            <nobr>{cell}</nobr>
-                        </div>)}
-                    </div>)}
-                </div>
-            </Fragment>}
-        </Fragment>;
+    fileReader.readAsText(supplierProductUploadFile);
+  };
+  processTextArea = textInput => {
+    let partLines = textInput.split('\n');
+    let uploadedData = [];
+    let splitCharacter = '\t';
+    partLines.forEach(partLine => uploadedData.push(partLine.split(splitCharacter)));
+    if (uploadedData.length > 0) {
+      this.setState({ uploadedData });
     }
-}
+  };
+  clearUploadData = () => {
+    this.setState({ uploadedData: [] });
+  };
 
+  render() {
+    const { modelName, uploadedData } = this.state;
+    const uploadData = uploadedData && uploadedData.length > 0;
+    const uploadDisabled = false;
+    return (
+      <Fragment key="supplierProductUploadFile">
+        {!uploadData && (
+          <div key="supplierProductUploadInput" className="grid">
+            <div className="grid-row">
+              <div className="grid-item--borderless field-label red align_right">Either:</div>
+              <div className="grid-item--borderless field-label">Select file for upload</div>
+              <div className="grid-item--borderless">
+                <input
+                  type="file"
+                  id="supplierProductUploadFile"
+                  accept=".csv"
+                  onChange={event => this.handleFileChosen(event.target.files[0])}
+                  disabled={uploadDisabled}
+                />
+              </div>
+            </div>
+            <div className="grid-row">
+              <div className="grid-item--borderless field-label red align_right">Or:</div>
+              <div className="grid-item--borderless field-label">
+                Paste a list of supplier products
+              </div>
+              <div className="grid-item--borderless">
+                <FormTextAreaInput
+                  title="Paste list of supplier part types and values here"
+                  placeholder={'e.g. Bars Deda 30cm'}
+                  onChange={this.processTextArea}
+                  disabled={!modelName}
+                  rows={20}
+                />
+              </div>
+            </div>
+          </div>
+        )}
+        {uploadData && (
+          <Fragment>
+            <div>
+              <Button key="supplierProductFileUploadCont" onClick={this.goToNextStep}>
+                Continue ...
+              </Button>
+              <Button key="supplierProductFileUploadReset" onClick={this.clearUploadData}>
+                Clear data
+              </Button>
+            </div>
+            <div
+              key="supplierProductUploadGrid"
+              className="grid"
+              style={{
+                height: window.innerHeight - 100 + 'px',
+                width: window.innerWidth - 50 + 'px',
+                overflow: 'scroll',
+              }}
+            >
+              <div key="supplierProductUploadHeaders" className="grid-row grid-row--header">
+                {supplierProductHeaders.map((cell, index) => (
+                  <div
+                    key={`col1${index}`}
+                    className={
+                      index === 0
+                        ? 'grid-item--header grid-header--fixed-left'
+                        : 'grid-item--header'
+                    }
+                  >
+                    {cell}
+                  </div>
+                ))}
+              </div>
+              {uploadedData.map((uploadedRow, rowIndex) => (
+                <div key={`detailRow${rowIndex}`} className="grid-row">
+                  {uploadedRow.map((cell, index) => (
+                    <div
+                      className={index === 0 ? 'grid-item grid-item--fixed-left' : 'grid-item'}
+                      key={`row${rowIndex}col${index}`}
+                    >
+                      <nobr>{cell}</nobr>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </Fragment>
+        )}
+      </Fragment>
+    );
+  }
+}
 
 export default SupplierProductUploadFile;

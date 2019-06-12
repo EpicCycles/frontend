f5d8eac42a1027789f56b10a173258f8a131f2ba
Anna <appdev.epiccycles@gmail.com>
2019-06-12 17:20:01 +0100

    procfile and package changes for heroku deploy

--- src/state/sagas/api.js
+++ src/state/sagas/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
-
+let baseUrl = '';
+if (process.env.NODE_ENV === 'production') {
+  baseUrl = process.env.REACT_APP_BASE_URL;
+}
 const instance = axios.create({
-  baseURL: '',
+  baseURL: baseUrl,
   timeout: 100000,
   headers: { 'Content-Type': 'application/json' },
 });

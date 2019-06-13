1b9af12572fb6fc192caa3ba7469ec1213d0b8b2
Anna <appdev.epiccycles@gmail.com>
2019-06-13 10:23:38 +0100

    proxy changes

--- src/state/sagas/api.js
+++ src/state/sagas/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 let baseUrl = '';
-if (process.env.NODE_ENV === 'production') {
-  baseUrl = process.env.REACT_APP_BASE_URL;
-}
+// if (process.env.NODE_ENV === 'production') {
+//   baseUrl = process.env.REACT_APP_BASE_URL;
+// }
 const instance = axios.create({
   baseURL: baseUrl,
   timeout: 100000,

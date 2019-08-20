8762ae95cba5eccc5a5d122b48482afced0761ff
Anna <appdev.epiccycles@gmail.com>
2019-08-20 15:00:48 +0100

    use common method to decide what to show

--- src/components/note/NoteGrid.jsx
+++ src/components/note/NoteGrid.jsx
@@ -4,11 +4,11 @@ import { customerNoteFields } from './helpers/noteFields';
 import ViewModel from '../app/model/ViewModel';
 import { noteActions } from './helpers/noteActions';
 import ModelTableHeaderRow from '../app/model/ModelTableHeaderRow';
+import { doWeHaveObjects } from '../../helpers/utils';
 
 const NoteGrid = props => {
   const { notes, users, deleteNote, saveNote } = props;
-  if (!Array.isArray(notes) || notes.length === 0)
-    return <div data-test="no-data-message">No Notes to view.</div>;
+  if (!doWeHaveObjects(notes)) return <div data-test="no-data-message">No Notes to view.</div>;
   return (
     <div className="grid-container">
       <div className="grid">
